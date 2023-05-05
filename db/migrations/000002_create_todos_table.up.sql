36826c9426c2d2878e5efb031924aeed0c52ca42
kitsune_yk <agvksl543@gmail.com>
2023-05-06 03:14:41 +0900

    feat: db処理を作成

---
+++ db/migrations/000002_create_todos_table.up.sql
@@ -0,0 +1,9 @@
+BEGIN;
+  CREATE TABLE IF NOT EXISTS todos(
+    id VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY,
+    text VARCHAR(255) ,
+    done BOOL DEFAULT FALSE,
+    user_id VARCHAR(255)
+  );
+  CREATE INDEX on todos(id);
+COMMIT;
